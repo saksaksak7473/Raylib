268289173b102da2ef922b7b200aa87f7324116d
saksaksak7473 <sakvansong059@gmail.com>
2026-08-18 16:47:50 +0700

    save

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ class Player:
     def __init__(self, x, y, w, h, color):
         self.pos = Vector2(x, y)
         self.dir = Vector2(1, 0)
-        self.speed = 100
+        self.speed = 1000
         self.w = w
         self.h = h
         self.color = color 
@@ -24,7 +24,21 @@ class Player:
         self.pos.x += self.dir.x * self.speed * dt
         self.pos.y += self.dir.y * self.speed * dt
         
+class Wall:
+    def __init__(self, x, y, w, h, color):
+        self.pos = Vector2(x, y)
+        self.w = w
+        self.h = h
+        self.color = color
+    
+    def draw(self):
+        draw_rectangle_v(self.pos, Vector2(self.w, self.h), self.color)
+        
 player = Player(0, 0, 50, 50, WHITE)
+walls = [
+    Wall(100, 100, 50, 200, WHITE),
+    Wall(150, 100, 400, 50, WHITE)
+]
     
 while not window_should_close():
     begin_drawing()
@@ -33,6 +47,8 @@ while not window_should_close():
     
     # Drawing
     clear_background(BLACK)
+    for wall in walls:
+        wall.draw()
     player.draw()
     
     end_drawing()
